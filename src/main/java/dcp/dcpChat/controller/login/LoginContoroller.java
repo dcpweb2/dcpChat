17387572c791a0471523afe1ebedf19a44aaf63e
inoKoichi <dcp_web02@yahoo.co.jp>
2017-09-22 18:20:25 +0900

    bootstrapをいれた

--- src/main/java/dcp/dcpChat/controller/login/LoginContoroller.java
+++ src/main/java/dcp/dcpChat/controller/login/LoginContoroller.java
@@ -9,33 +9,29 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody; // 画面上に文字列のみを表示させる時に使う。
 import dcp.dcpChat.form.login.LoginForm;
 
-
 @Controller
-@EnableAutoConfiguration
 public class LoginContoroller {
-	
+    
     @ModelAttribute
-    LoginForm setUpForm() {
+    LoginForm setLoginForm() {
         return new LoginForm();
     }
-    
-	private static final Logger logger = LoggerFactory.getLogger( LoginContoroller.class);
-	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginContoroller.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String sample(Locale locale, Model model) {
-    	logger.info("Welcome home! The client locale is {}.", locale);
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "login/login";
+        logger.info("Welcome home! The client locale is {}.", locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("serverTime", formattedDate);
+        
+        return "login/login";
     }
 }
